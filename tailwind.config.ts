738b4a777d97a99afc850821f3c4f441f6e4e452
Harsh Sandhu <nyxfor13days@gmail.com>
2024-11-02 11:29:07 +0000

    Add custom sans font family to Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -61,6 +61,9 @@ const config: Config = {
   				ring: 'hsl(var(--sidebar-ring))'
   			}
   		},
+			fontFamily: {
+				sans: 'var(--font-sans)',
+			},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
