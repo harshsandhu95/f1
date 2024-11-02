ebb214413eb45c71907e488d2c804d1c4e7ef002
Harsh Sandhu <nyxfor13days@gmail.com>
2024-11-02 21:07:30 +0000

    Update types for CarData and add RaceControl and Position types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,11 +4,11 @@ export type NavItems = {
   title: string;
   url: string;
   icon?: React.ForwardRefExoticComponent<Omit<LucideProps, "ref"> & React.RefAttributes<SVGSVGElement>>;
-}
+};
 
 export type CarData = {
   brake: number;
-  date: Date;
+  date: string;
   driver_number: number;
   drs: number;
   meeting_key: number;
@@ -17,4 +17,27 @@ export type CarData = {
   session_key: number;
   speed: number;
   throttle: number;
+};
+
+export type RaceControl = {
+  session_key: number;
+  meeting_key: number;
+  date: string;
+  category: string;
+  flag: string;
+  lap_number: number;
+  message: string;
+  driver_number: number;
+  scope: string;
+  sector: number;
+};
+
+export type Position = {
+  session_key: number;
+  meeting_key: number;
+  driver_number: number;
+  date: string;
+  position: number;
+  initial_position: number;
+  final_position: number;
 };
